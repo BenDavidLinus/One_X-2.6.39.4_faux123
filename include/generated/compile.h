614583394ee542746cb8256d09a2811830938542
faux123 <reioux@gmail.com>
2012-07-06 10:57:25 -0700

    latest beta source

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 100 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#100 SMP PREEMPT Thu Jul 5 11:04:58 MST 2012"
+#define LINUX_COMPILE_TIME "11:04:58"
+#define LINUX_COMPILE_BY "faux123"
+#define LINUX_COMPILE_HOST "localhost"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.39.4-gc9932b1"
